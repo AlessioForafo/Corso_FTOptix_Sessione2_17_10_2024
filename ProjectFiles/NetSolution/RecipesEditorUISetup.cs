be5c57f960171310380702a47ea6b7d25365faab
MassimoV <massimo.variolo@rockwellautomation.com>
2024-10-17 14:44:02 +0200

    localization & OPC-UA

--- ProjectFiles/NetSolution/RecipesEditorUISetup.cs
+++ ProjectFiles/NetSolution/RecipesEditorUISetup.cs
@@ -15,6 +15,7 @@ using System.Collections.Generic;
 using FTOptix.SQLiteStore;
 using FTOptix.Store;
 using FTOptix.CommunicationDriver;
+using FTOptix.OPCUAServer;
 #endregion
 
 public class RecipesEditorUISetup : BaseNetLogic
